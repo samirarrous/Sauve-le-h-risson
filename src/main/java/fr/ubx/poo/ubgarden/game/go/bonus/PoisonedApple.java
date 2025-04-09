4654954429c2795c431523990494c3f0a73c5e18
Patthys <matthysmontet@gmail.com>
2025-04-09 14:42:55 +0200

    Adding Bonnus / ennemies Sckeletton creating Door and ennemies Packages

---
+++ src/main/java/fr/ubx/poo/ubgarden/game/go/bonus/PoisonedApple.java
@@ -0,0 +1,10 @@
+package fr.ubx.poo.ubgarden.game.go.bonus;
+
+import fr.ubx.poo.ubgarden.game.Position;
+import fr.ubx.poo.ubgarden.game.go.decor.Decor;
+
+public class PoisonedApple extends Bonus{
+    public PoisonedApple(Position position, Decor decor) {
+        super(position, decor);
+    }
+}
---
+++ src/main/java/fr/ubx/poo/ubgarden/game/go/decor/ennemies/Guepe.java
@@ -0,0 +1,10 @@
+package fr.ubx.poo.ubgarden.game.go.decor.ennemies;
+
+import fr.ubx.poo.ubgarden.game.Position;
+import fr.ubx.poo.ubgarden.game.go.decor.Decor;
+
+public class Guepe extends Decor {
+    public Guepe(Position position) {
+        super(position);
+    }
+}
